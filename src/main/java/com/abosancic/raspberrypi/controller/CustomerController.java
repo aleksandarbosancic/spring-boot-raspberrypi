c3589969d7974e1c88499791cfb329ca5f65fe76
Aleksandar Bosancic <aleksandar.bosancic@devlabs.ba>
2018-10-29 14:43:53 +0100

    Add simple endpoint for find customer by id

--- src/main/java/com/abosancic/raspberrypi/controller/CustomerController.java
+++ src/main/java/com/abosancic/raspberrypi/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.abosancic.raspberrypi.entity.Customer;
 import com.abosancic.raspberrypi.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ class CustomerController {
    @GetMapping("/all")
    public List<Customer> getAllCustomers() {
       return customerService.getAllCustomers();
+   }
+
+   @GetMapping("/byId/{id}")
+   public Customer getCustomerById( @PathVariable Long id ) {
+      return customerService.getCustomerById(id);
    }
 }
